b306830ddd1d66ecc2a12d53645bc8ade7b5b21e
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-08-25 15:02:55 +0200

    Rename test files.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 using SafeTestsets
 
 @safetestset "Utility Functions                                                               " begin include("utils_tests.jl") end
-@safetestset "General Equation Functionality                                                  " begin include("general_equations_tests.jl") end
-@safetestset "Differential Equations                                                          " begin include("differential_equations_tests.jl") end
+@safetestset "Abstract Equations                                                              " begin include("geometric_equations_tests.jl") end
+@safetestset "Ordinary Differential Equations                                                 " begin include("ordinary_differential_equations_tests.jl") end
 @safetestset "Differential Algebraic Equations                                                " begin include("differential_algebraic_equations_tests.jl") end
-@safetestset "Stochastic Equations                                                            " begin include("stochastic_equations_tests.jl") end
+@safetestset "Stochastic Differential Equations                                               " begin include("stochastic_differential_equations_tests.jl") end
