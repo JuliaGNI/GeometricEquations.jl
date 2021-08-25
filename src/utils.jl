590f70f4133b2f9298376650921c84b5f0b76767
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-08-25 15:10:42 +0200

    Add promote_tspan function (adapted from SciMLBase.jl).

--- src/utils.jl
+++ src/utils.jl
@@ -3,6 +3,22 @@ function_dummy_v(t, q, v) = nothing
 function_dummy_v(t, q, p, v) = nothing
 
 
+"""
+    promote_tspan(tspan)
+Convert the `tspan` field of a `GeometricProblem` to a `(tmin, tmax)` tuple, where both
+elements are of the same type.
+"""
+promote_tspan((t1,t2)::Tuple{T,S}) where {T,S} = promote(t1, t2)
+promote_tspan(tspan::Number) = (zero(tspan),tspan)
+promote_tspan(tspan::Nothing) = (nothing,nothing)
+promote_tspan(tspan::AbstractArray) = length(tspan) == 2 ? promote_tspan((first(tspan),last(tspan))) : throw(error("The length of tspan must be two (and preferably, tspan should be a tuple, i.e. (0.0,1.0))."))
+
+function promote_tspan_and_tstep((t1,t2)::Tuple, Δt::Number)
+    t = promote(t1, t2, Δt)
+    return (t[1], t[2]), t[3]
+end
+
+
 function initial_multiplier(q₀::AbstractVector{DT}, λ₀::AbstractVector{DT}) where {DT <: Number}
     zero(λ₀)
 end
--- test/utils_tests.jl
+++ test/utils_tests.jl
@@ -1,11 +1,27 @@
 
-using GeometricEquations: function_dummy_v, initial_multiplier, symplectic_matrix
+using GeometricEquations: function_dummy_v, promote_tspan, initial_multiplier, symplectic_matrix
 using Test
 
 include("initial_conditions.jl")
 
 @test function_dummy_v(t₀, q₀, λ₀) === nothing
 @test function_dummy_v(t₀, q₀, p₀, λ₀) === nothing
+
+
+@test promote_tspan(0) == (0,0)
+@test promote_tspan(1) == (0,1)
+@test promote_tspan(0.0) == (0.0,0.0)
+@test promote_tspan(1.0) == (0.0,1.0)
+@test promote_tspan((0,   1.0)) == (0.0,1.0)
+@test promote_tspan((0.0, 1  )) == (0.0,1.0)
+@test promote_tspan([0,   1.0]) == (0.0,1.0)
+@test promote_tspan([0.0, 1  ]) == (0.0,1.0)
+@test promote_tspan([0,   1  ]) == (0,  1  )
+@test promote_tspan([0.0, 1.0]) == (0.0,1.0)
+
+@test_throws ErrorException promote_tspan([0.0, 0.5, 1.0])
+
+
 zero_vec = [zeros(3) for i in 1:3]
 
 @test initial_multiplier(zeros(3), zeros(3)) == zeros(3)
