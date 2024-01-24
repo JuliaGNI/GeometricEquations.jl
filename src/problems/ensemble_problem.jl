ba7f634b112d9f819530d3dd63123de46d937b71
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-01-24 14:46:20 +0100

    Add docstring for EnsembleProblem.

--- src/problems/ensemble_problem.jl
+++ src/problems/ensemble_problem.jl
@@ -1,5 +1,61 @@
 """
-EnsembleProblem: stores a GeometricEquation together with multiple sets of initial conditions, parameters, time span and time step size.
+EnsembleProblem: stores a GeometricEquation together with multiple sets of initial conditions and/or parameters, a time span for integration and a time step size.
+
+An `EnsembleProblem` is initialized by providing a `GeometricEquation`, an integration time span (typically a tuple with two values, start and end time, respectively), a timestep, and one of the three following options:
+* a vector of initial conditions and a vector of parameter sets, with both vectors having the same length,
+* a vector of initial conditions and a single set of parameters,
+* a single initial condition and a vector of parameter sets.
+
+Each initial condition is a `NamedTuple` that contains one field for each state variable.
+Each parameter set is either a `NamedTuple` containing the equation's parameters or `NullParameters` indicating that the equation does not have any parameters.
+
+The different constructors then generate two vectors, one for the initial conditions and one for the parameters, where each pair of the corresponding entries defines one problem.
+In the first case, the respective constructor just checks if both vectors are of the same size.
+In the second case, the respective constructor creates a parameter vector, where each entry holds the same parameter set.
+In the third case, the respective constructor creates an initial condition vector, where each entry holds the same initial conditions.
+One may be inclined to think that the first and second constructor lead to a waste of memory, but in reality the respective vectors only hold references to the same initial conditons or parameters.
+Thus the data is not actually duplicated.
+
+### Parameters
+
+* `ST <: GeometricEquation`: super type, used for dispatch
+* `DT <: Number`: data type
+* `TT <: Real`: time step type
+* `AT <: AbstractArray{DT}`: array type of state variable
+* `equType <: GeometricEquation`: equation type
+* `functionsType <: NamedTuple`: types of all function methods
+* `solutionsType <: NamedTuple`: types of all solution methods
+* `icsType <: AbstractVector{<:NamedTuple}`: types of all initial conditions 
+* `parType <: AbstractVector{<:OptionalParameters}`: parameters type
+
+### Fields
+
+* `equation`: reference to the parent equation object holding the vector fields, etc.
+* `functions`: methods for all vector fields, etc., that define the problem
+* `solutions`: methods for all solutions, etc., if defined
+* `tspan`: time span for problem `(t₀,t₁)`
+* `tstep`: time step to be used in simulation
+* `ics`: vector of `NamedTuple` containing the initial conditions, each `NamedTuple` must contain one field for each state variable
+* `parameters`: vector of either `NamedTuple` containing the equation's parameters or `NullParameters` indicating that the equation does not have any parameters
+
+### Constructors
+
+The `EnsembleProblem` provides the following constructors:
+```
+EnsembleProblem(equ, tspan, tstep, ics::AbstractVector{<:NamedTuple}, parameters::AbstractVector{<:OptionalParameters})
+EnsembleProblem(equ, tspan, tstep, ics::AbstractVector{<:NamedTuple}, parameters::OptionalParameters=NullParameters())
+EnsembleProblem(equ, tspan, tstep, ics::NamedTuple, parameters::AbstractVector{<:OptionalParameters})
+EnsembleProblem(equ, tspan, tstep, ics, ::Nothing) = 
+    EnsembleProblem(equ, tspan, tstep, ics, NullParameters())
+EnsembleProblem(equ, tspan, tstep, ics; parameters = NullParameters()) = 
+    EnsembleProblem(equ, tspan, tstep, ics, parameters)
+```
+
+* `equ` is a subtype of `GeometricEquation`
+* `tspan` is a tuple `(t₀,t₁)` of the integration time span with `t₀` the start time and `t₁` the end time
+* `tstep` is the time step, typically a value of some `AbstractFloat` subtype
+* `ics` are the initial conditions, either a single set or a vector of multiple sets
+* `parameters` are the static parameters of the problem, either a single set or a vector of multiple sets
 
 """
 struct EnsembleProblem{superType <: GeometricEquation, dType <: Number, tType <: Real,
@@ -41,24 +97,24 @@ function EnsembleProblem(equ::equType, tspan, tstep, ics::AbstractVector{<:Named
     EnsembleProblem{superType, dType, tType, arrayType, equType, typeof(funcs), typeof(sols), typeof(ics), typeof(parameters)}(equ, funcs, sols, _tspan, _tstep, ics, parameters)
 end
 
-function EnsembleProblem(equ, tspan, tstep, ics::AbstractVector{<:NamedTuple}, params::OptionalParameters=NullParameters())
-    _params = similar(ics, typeof(params))
+function EnsembleProblem(equ, tspan, tstep, ics::AbstractVector{<:NamedTuple}, parameters::OptionalParameters=NullParameters())
+    _params = similar(ics, typeof(parameters))
 
     for i in eachindex(_params)
-        _params[i] = params
+        _params[i] = parameters
     end
 
     EnsembleProblem(equ, tspan, tstep, ics, _params)
 end
 
-function EnsembleProblem(equ, tspan, tstep, ics::NamedTuple, params::AbstractVector{<:OptionalParameters})
-    _ics = similar(params, typeof(ics))
+function EnsembleProblem(equ, tspan, tstep, ics::NamedTuple, parameters::AbstractVector{<:OptionalParameters})
+    _ics = similar(parameters, typeof(ics))
 
     for i in eachindex(_ics)
         _ics[i] = ics
     end
 
-    EnsembleProblem(equ, tspan, tstep, _ics, params)
+    EnsembleProblem(equ, tspan, tstep, _ics, parameters)
 end
 
 function EnsembleProblem(equ, tspan, tstep, ics, ::Nothing)
